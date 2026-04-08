befcad8d885daa077a32b54783dc9ab1ba99e39c
mrbisme <mbushby41@gmail.com>
2026-04-08 19:49:55 -0400

    Added Chisel

--- src/main/java/net/mrbisme/tutorialmod/item/ModItems.java
+++ src/main/java/net/mrbisme/tutorialmod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.mrbisme.tutorialmod.TutorialMod;
+import net.mrbisme.tutorialmod.item.custom.ChiselItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,6 +17,9 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
