f41f58e8f853e780bda325e9c4cf1741d06bb84c
mrbisme <mbushby41@gmail.com>
2026-04-11 16:24:01 -0400

    Setup datagen

---
+++ src/main/java/net/mrbisme/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,31 @@
+package net.mrbisme.tutorialmod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.mrbisme.tutorialmod.TutorialMod;
+import net.mrbisme.tutorialmod.item.ModItems;
+import net.mrbisme.tutorialmod.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pLookupProvider,
+                              CompletableFuture<TagLookup<Block>> pBlockTags, @Nullable ExistingFileHelper existingFileHelper) {
+        super(pOutput, pLookupProvider, pBlockTags, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.ALEXANDRITE.get())
+                .add(ModItems.RAW_ALEXANDRITE.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+    }
+}
